f2876dea40325e234e3c62d4d13082ec440ba644
Rick Ross <rross@pivotal.io>
2016-09-12 19:32:59 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+	   var config = new ConfigurationBuilder()
+    		.AddCommandLine(args)
+    		.Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+		.UseConfiguration(config)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
